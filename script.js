e4adcd614e6365e8f36d649d9c6d5a38264d6669
dahyungryu <dahyung02@gmail.com>
2024-11-06 15:33:56 +0900

    타이머 구현-html

---
+++ script.js
@@ -0,0 +1,13 @@
+const hrs = document.querySelector('#hrs');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+
+const startPauseBtn = document.querySelector('#start-pause')
+
+function updateTime() {
+    let hours =  parseInt(hrs.value) || 0;
+    let minutes = parseInt(min.value) || 0;
+    let seconds = parseInt(sec.value) || 0;
+
+
+}
